f87463e66f4b85bdffe4468d124ca8ae3c92b87a
Christian Georgeot <christian.georgeot@gmail.com>
2023-11-16 19:04:32 +0100

    Transformation des flèches de navigation en boutons

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,16 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+/**
+ * Change carousel left and right arrows into previous
+ * and next buttons adding click event listeners 
+ */
+const previous_button = document.querySelector("#banner .arrow_left")
+previous_button.addEventListener("click", () => {
+    console.log("Previous image")
+})
+const next_button = document.querySelector("#banner .arrow_right")
+next_button.addEventListener("click", () => {
+    console.log("Next image")
+})
