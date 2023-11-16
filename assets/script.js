731922d3635790426473a06046ed5d04018a36fa
Christian Georgeot <christian.georgeot@gmail.com>
2023-11-16 22:52:41 +0100

    Ajout de la logique de fonctionnement du carousel

--- assets/script.js
+++ assets/script.js
@@ -20,27 +20,13 @@ const slides = [
 // index of the current slide
 let current_slide = 0
 
-display_dots()
-
-/**
- * Change carousel left and right arrows into previous
- * and next buttons adding click event listeners 
- */
-const previous_button = document.querySelector("#banner .arrow_left")
-previous_button.addEventListener("click", () => {
-    console.log("Previous image")
-})
-const next_button = document.querySelector("#banner .arrow_right")
-next_button.addEventListener("click", () => {
-    console.log("Next image")
-})
-
 /**
  * Display the index of current image among the full
  * list of carousel images designed with empty dots
  */
 function display_dots() {
     const dots = document.querySelector(".dots")
+    dots.innerHTML = ''
     for(i = 0; i < slides.length; i++) {
         if (current_slide === i) {
 
@@ -53,3 +39,57 @@ function display_dots() {
         }
     }
 }
+
+/**
+ * Display image and tag line according to
+ * the current index of the carousel 
+ */
+function display_slide() {
+
+    // Display image
+    const image = document.querySelector("#banner .banner-img")
+    image.src = `./assets/images/slideshow/${slides[current_slide].image}`
+
+    // Display title
+    const title = document.querySelector("#banner p")
+    title.innerHTML = slides[current_slide].tagLine
+}
+
+/**
+ * Update User Interface
+ */
+function update_UI() {
+    display_dots()
+    display_slide()
+}
+
+/**
+ * Change carousel left and right arrows into previous
+ * and next buttons adding click event listeners 
+ */
+const previous_button = document.querySelector("#banner .arrow_left")
+previous_button.addEventListener("click", () => {
+    console.log("Previous image")
+    if (current_slide === 0) {
+        current_slide = slides.length - 1
+    } else {
+        current_slide--
+    }
+    update_UI()
+})
+
+const next_button = document.querySelector("#banner .arrow_right")
+next_button.addEventListener("click", () => {
+    console.log("Next image")
+    if (current_slide === slides.length - 1) {
+        current_slide = 0
+    } else {
+        current_slide++
+    }
+    update_UI()
+})
+
+/**
+ * Main
+ */
+update_UI()
