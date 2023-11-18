e234a648508bbae86c5c816a3846e82dfa8d7171
Christian Georgeot <christian.georgeot@gmail.com>
2023-11-18 14:50:09 +0100

    Conformité cahier des charges et commentaires

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,47 @@
+/*========================================================
+    Data model
+========================================================*/
+
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+    {
+        "image":"slide1.jpg",
+        "tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image":"slide2.jpg",
+        "tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image":"slide3.jpg",
+        "tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image":"slide4.png",
+        "tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+    }
 ]
 
 // index of the current slide
 let current_slide = 0
 
+/*========================================================
+    Internal functions
+========================================================*/
+
 /**
- * Display the index of current image among the full
- * list of carousel images designed with empty dots
- */
+* Display the bullet of current image among the full
+* list of carousel images designed with empty bullets
+*/
 function display_dots() {
     const dots = document.querySelector(".dots")
     dots.innerHTML = ''
     for(i = 0; i < slides.length; i++) {
         if (current_slide === i) {
-
+            
             // current image with full dot
             dots.innerHTML += '<div class="dot dot_selected"></div>'
         } else {
-
+            
             // carousel image with empty dot
             dots.innerHTML += '<div class="dot"></div>'
         }
@@ -41,55 +49,44 @@ function display_dots() {
 }
 
 /**
- * Display image and tag line according to
- * the current index of the carousel 
- */
+* Display image and tag line according to
+* the current index in the carousel 
+*/
 function display_slide() {
-
+    
     // Display image
     const image = document.querySelector("#banner .banner-img")
     image.src = `./assets/images/slideshow/${slides[current_slide].image}`
-
+    
     // Display title
     const title = document.querySelector("#banner p")
     title.innerHTML = slides[current_slide].tagLine
 }
 
-/**
- * Update User Interface
- */
-function update_UI() {
-    display_dots()
-    display_slide()
-}
+/*========================================================
+    Run at first launch
+========================================================*/
 
 /**
- * Change carousel left and right arrows into previous
- * and next buttons adding click event listeners 
- */
+* Change carousel left and right arrows into previous
+* and next buttons adding click event listeners 
+*/
 const previous_button = document.querySelector("#banner .arrow_left")
 previous_button.addEventListener("click", () => {
-    console.log("Previous image")
-    if (current_slide === 0) {
-        current_slide = slides.length - 1
-    } else {
-        current_slide--
-    }
-    update_UI()
+    if (current_slide > 0) { current_slide-- } 
+    else { current_slide = slides.length - 1 }
+    display_slide()
+    display_dots()
 })
 
 const next_button = document.querySelector("#banner .arrow_right")
 next_button.addEventListener("click", () => {
-    console.log("Next image")
-    if (current_slide === slides.length - 1) {
-        current_slide = 0
-    } else {
-        current_slide++
-    }
-    update_UI()
+    if (current_slide < slides.length) { current_slide++ } 
+    else { current_slide = 0 }
+    display_slide()
+    display_dots()
 })
 
-/**
- * Main
- */
-update_UI()
+// Add missing bullet points on pre-existing carousel image
+display_dots()
+
