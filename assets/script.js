cbf57f257dfb917f220e492ca28eafd1b430e4ec
Christian Georgeot <christian.georgeot@gmail.com>
2023-11-18 16:24:05 +0100

    Ajout des ';' après chaque instruction

--- assets/script.js
+++ assets/script.js
@@ -22,31 +22,11 @@ const slides = [
 ]
 
 // index of the current slide
-let current_slide = 0
+let current_slide = 0;
 
 /*========================================================
     Internal functions
 ========================================================*/
-
-/**
-* Display the bullet of current image among the full
-* list of carousel images designed with empty bullets
-*/
-function display_dots() {
-    const dots = document.querySelector(".dots")
-    dots.innerHTML = ''
-    for(i = 0; i < slides.length; i++) {
-        if (current_slide === i) {
-            
-            // current image with full dot
-            dots.innerHTML += '<div class="dot dot_selected"></div>'
-        } else {
-            
-            // carousel image with empty dot
-            dots.innerHTML += '<div class="dot"></div>'
-        }
-    }
-}
 
 /**
 * Display image and tag line according to
@@ -55,12 +35,32 @@ function display_dots() {
 function display_slide() {
     
     // Display image
-    const image = document.querySelector("#banner .banner-img")
-    image.src = `./assets/images/slideshow/${slides[current_slide].image}`
+    const image = document.querySelector("#banner .banner-img");
+    image.src = `./assets/images/slideshow/${slides[current_slide].image}`;
     
     // Display title
-    const title = document.querySelector("#banner p")
-    title.innerHTML = slides[current_slide].tagLine
+    const title = document.querySelector("#banner p");
+    title.innerHTML = slides[current_slide].tagLine;
+}
+
+/**
+* Display the bullet of current image among the full
+* list of carousel images designed with empty bullets
+*/
+function display_dots() {
+    const dots = document.querySelector(".dots");
+    dots.innerHTML = '';
+    for(i = 0; i < slides.length; i++) {
+        if (current_slide === i) {
+            
+            // current image with full dot
+            dots.innerHTML += '<div class="dot dot_selected"></div>';
+        } else {
+            
+            // carousel image with empty dot
+            dots.innerHTML += '<div class="dot"></div>';
+        }
+    }
 }
 
 /*========================================================
@@ -71,22 +71,22 @@ function display_slide() {
 * Change carousel left and right arrows into previous
 * and next buttons adding click event listeners 
 */
-const previous_button = document.querySelector("#banner .arrow_left")
+const previous_button = document.querySelector("#banner .arrow_left");
 previous_button.addEventListener("click", () => {
-    if (current_slide > 0) { current_slide-- } 
-    else { current_slide = slides.length - 1 }
-    display_slide()
-    display_dots()
+    if (current_slide > 0) { current_slide--; } 
+    else { current_slide = slides.length - 1; }
+    display_slide();
+    display_dots();
 })
 
-const next_button = document.querySelector("#banner .arrow_right")
+const next_button = document.querySelector("#banner .arrow_right");
 next_button.addEventListener("click", () => {
-    if (current_slide < slides.length - 1) { current_slide++ } 
-    else { current_slide = 0 }
-    display_slide()
-    display_dots()
+    if (current_slide < slides.length - 1) { current_slide++; } 
+    else { current_slide = 0; }
+    display_slide();
+    display_dots();
 })
 
 // Add missing bullet points on pre-existing carousel image
-display_dots()
+display_dots();
 
